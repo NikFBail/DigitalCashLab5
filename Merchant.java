392acb983c2a3a00654811386a7ca782ed4f029d
Nik Bailey <bail0301@morris.umn.edu>
2022-11-21 09:58:08 -0600

    fixed comments

--- Merchant.java
+++ Merchant.java
@@ -39,38 +39,30 @@ public class Merchant {
     ai, ci, and yi for a 1.
     3. Use those inputs to validate f(xi,yi) and make sure its not double spent.
     4. If it is valid and has not been double spent, deposit in the merchant bank account.
-     */
-    /*
+
     - ai is a binary number the same length as I, which is the customer account number concatenated with the bill number,
      so 18 digits.
 
     - ci and di are both random numbers for each chunk
-     */
 
-    /*  Tasks:
+    Tasks:
     implement SHA
     use BigInteger
     method getBytes of string class for SHA
 
-        Details:
+    Details:
     key ~ 1000 bit modulus, generate 500-bit or so primes
     account number is 8 digits, bill number will be 10 digits
     a,c,d are between 0 and 2^(128)-1
     Use SHA-256 on concatenation of inputs for the XOR with a and padded with zeros on the left to 128 bits
     Has functions f,g use result of SHA-256 on the concatenation of the two inputs in the order given in the description
     k = k' = 10 for testing
-     */
 
-    /*
     As merchants, we only verify cash is valid and deposit the cash in the bank.
     We also check with the bank to make sure the bill number, x, hasn't been used before.
-     */
 
-    /*
     Customers send merchants (x, f(x)^d).  We verify by calculating f(x) = f(x)^d^e using the bank's public exponent e.
-     */
 
-    /*
     We verify by calculating f(xi, yi) for each chunk, finding the product, and then checking if the
     product is equal to f(xi, yi)^d^e, because we get ample information from either choice
      */
